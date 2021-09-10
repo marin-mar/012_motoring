2bf02da937d30cd892db5759155b94f69608fcc4
marin-mar <72693639+marin-mar@users.noreply.github.com>
2021-09-10 18:52:40 +0300

    add load data by title of product

--- src/js/_products.js
+++ src/js/_products.js
@@ -1,20 +1,11 @@
 /* products */
+
+/* get products data from json */
 const productsMoreBtn = document.querySelector(".products__btn.btn--more");
 const productsAllBtn = document.querySelector(".products__btn.btn--all");
-const productsTitles = document.querySelectorAll(".products__title");
 const productsItems = document.querySelectorAll(".products__item");
 const productsList = document.querySelector(".products__list");
 const productsDataFile = "../json/products.json";
-
-productsTitles.forEach((productsTitle) => {
-  productsTitle.addEventListener("click", (e) => {
-    e.preventDefault();
-    console.log("productsTitle clicked");
-    console.log(e.target.firstChild.nodeValue);
-    // let width = productsTitle.getBoundingClientRect().width;
-    // console.log(width);
-  });
-});
 
 productsMoreBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -52,7 +43,8 @@ let showPerClick = 3;
 function loadProductsData(data, btn) {
   productsLength = data.length;
   for (let i = 0; i < data.length; i++) {
-    products.push(`<li data-prodid="${data[i].id}" class="products__item">
+    let product = `
+    <li data-prodid="${data[i].id}" class="products__item">
     	<img class="products__img" src="${data[i].img}" alt="${data[i].title}" width="260" height="300" />
     	<h3 class="products__subtitle">${data[i].title}</h3>
     	<p class="products__text">${data[i].text}</p>
@@ -60,7 +52,14 @@ function loadProductsData(data, btn) {
     		<span class="products__price--new">${data[i].newprice}</span>
     		<span class="products__price--old">${data[i].oldprice}</span>
     	</p>
-    </li>`);
+    </li>`;
+    products.push(product);
+
+    if (btn.classList.contains("products__title")) {
+      if (btn.firstChild.nodeValue == data[i].group) {
+        productsList.insertAdjacentHTML("beforeend", product);
+      }
+    }
   }
 
   if (btn.classList.contains("btn--all")) {
@@ -87,4 +86,23 @@ function loadProductsData(data, btn) {
   }
 
   return products;
+}
+
+/* load data by categories */
+
+const productsTitles = document.querySelectorAll(".products__title");
+
+if (productsTitles.length > 0) {
+  productsTitles.forEach((productsTitle) => {
+    productsTitle.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      productsTitles.forEach((elem) => elem.classList.remove("btn--active"));
+      productsTitle.classList.add("btn--active");
+
+      productsList.textContent = "";
+      console.log(productsTitle.firstChild.nodeValue);
+      getProductsData(productsTitle);
+    });
+  });
 }
